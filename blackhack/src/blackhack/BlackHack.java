e8b1bb777b649bbff2971a4cbf30b3bda5ea6bb4
Sebastian Edholm <sebastian.edholm@gmail.com>
2018-12-19 15:03:11 +0100

    some oop

--- blackhack/src/blackhack/BlackHack.java
+++ blackhack/src/blackhack/BlackHack.java
@@ -6,13 +6,5 @@ public class BlackHack {
 
 	public static void main(String[] args) {
 		ArrayDeque<Deck> decks = new ArrayDeque<Deck>();
-		decks.add(new Deck());
-		decks.add(new Deck());
-		decks.add(new Deck());
-		
-		System.out.println(decks.element().drawCardFromDeck().toString());
-
-		
-
 	}
 }
