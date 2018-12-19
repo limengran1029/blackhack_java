281d27a94fa9dd42780f2ac8cd94bf1457c5323c
Hendrikx <mathias_hendrikx@hotmail.com>
2018-12-19 11:20:57 +0100

    Testar igen..

--- blackhack/BlackHack.java
+++ blackhack/BlackHack.java
@@ -4,6 +4,10 @@ public class BlackHack {
 	public static void main(String[] args) {
 		System.out.println("Mathias");
 		System.out.println("Mathias");
+		
+		for (i = 0; i < 10; i++) {
+			System.out.println("det här med git...");
+		}
 	}
 
 }
