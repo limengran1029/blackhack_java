b01b9a7e285838b9e01340e05380ecd0354cacad
Sebastian Edholm <sebastian.edholm@gmail.com>
2018-12-20 09:32:48 +0100

    revert2

--- blackhack/src/blackhack/Deck.java
+++ blackhack/src/blackhack/Deck.java
@@ -1,4 +1,20 @@
 package blackhack;
 
+import java.util.ArrayDeque;
+
 public class Deck {
+	private int NUM_CARDS = 52;
+	private ArrayDeque<Card> deck = new ArrayDeque<Card>();
+
+	public Deck() {
+		for (int i = 1; i < NUM_CARDS - 1; i++)
+		{
+			deck.add(new Card());
+		}
+	}
+	
+	public Card drawCardFromDeck() {
+		return deck.pop();
+	}
+
 }
