3bbce2017a86f92a57dd3e4ff86bf68d0943175e
Sebastian Edholm <sebastian.edholm@gmail.com>
2018-12-20 09:32:40 +0100

    revert

--- blackhack/src/blackhack/Card.java
+++ blackhack/src/blackhack/Card.java
@@ -1,17 +1,4 @@
 package blackhack;
 
 public class Card {
-	private int value;
-	private String color;
-	
-	public Card() {
-		value = 1;
-		color = "gray";
-	}
-	
-	public String toString() {
-		return color;
-	}
-	
-
 }
