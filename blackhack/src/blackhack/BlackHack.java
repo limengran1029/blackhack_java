eb7afe5a54fbd1b3dbcc2c5c71b92d89dceea39b
tobiasgronberg <tobias.groenberg@gmail.com>
2018-12-20 13:47:44 +0100

    updated blackhack mainmenu

--- blackhack/src/blackhack/BlackHack.java
+++ blackhack/src/blackhack/BlackHack.java
@@ -6,7 +6,7 @@ public class BlackHack {
 
 	public static void main(String[] args) {
 
-		System.out.println("V�lkommen till BlackJack\n [1] Logga in\n [2] Registrera\n [3] Avsluta");
+		System.out.println("Welcome to Blackhack!\n[1]Login\n[2]Register\n[3]Exit");
 
 		Scanner inp = new Scanner(System.in);
 		String choice = inp.next();
@@ -15,34 +15,37 @@ public class BlackHack {
 		int fullclearence = 0; // S�tts till 1 eller 0 returneras fr�n metod i databaser.
 		int accepted = 0; // S�tts till 1 eller 0 returneras fr�n en metod i databaser.
 		
+		
 		while (true) 
 		{
 			if (choice.equals("1"))
 			{
-				while(true) {
+				while(true) 
+				{
 					// Kod f�r inloggning
-					System.out.println("Skriv in ett anv�ndarnamn:");
+					System.out.println("Enterusername: ");
 					username = inp.next();
 					// Kolla om anv�ndarnamn redan �r taget
 					if (accepted == 1)
 					{
-						System.out.println("Skriv in ett l�senord");
+						System.out.println("Enter password: ");
 						password = inp.next();
 						// kolla om l�senord st�mmer med anv�ndare
 						if (fullclearence == 1)
 						{
-							System.out.println("V�lkommen + h�r skriver vi in namn taget fr�n anv�ndare.");
+							System.out.println("Welcome! " + username + " Have fun Blackhacking!");
 							break;
 						}
 						else
 						{
-							System.out.println("Fel l�senord!");
+							System.out.println("Wrong password!");
 							break;
 						}
 					}
 					else
 					{
-						System.out.println("Fel anv�ndarnamn!");
+						System.out.println("Wrong username!");
+						break;
 					}
 				}
 				
@@ -51,21 +54,49 @@ public class BlackHack {
 			}
 			else if (choice.equals("2"))
 			{
+				while(true) 
+				{
+					System.out.println("Enter desired Username: ");
+					username = inp.next();
+					
+					if(accepted==1)
+					{
+				
+					System.out.println("Enter desired Password: ");
+					password = inp.next();
+					
+					if(fullclearence==1)
+					{
+						System.out.println(username + " your account has been created, have fun playing!");
+						break;
+					}
+					else
+						System.out.println("no password entered");
+						break;
+				
+					}
+				else
+					System.out.println("Username exists already!");
+					break;
+				}	
+				
+				
+				
 				// kod f�r registrering
 			}
 			else if (choice.equals("3"))
 			{
-				System.out.println("Du valde att avsluta programmet...");
+				System.out.println("You're exiting the program..\nGoodbye and please come again!");
 				break;
 			}
 			else
 			{
-				System.out.println("V�nligen v�lj ett korrect alternativ");
+				System.out.println("Please enter correct menu choice!");
 			}
 		}
 			
 			
-			
+			inp.close();
 	}
 			
 
