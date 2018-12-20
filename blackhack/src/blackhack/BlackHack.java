a37b8dcd7c4718a989f7ae8cec5880199482a3a3
Hendrikx <mathias_hendrikx@hotmail.com>
2018-12-20 14:25:57 +0100

    Updated 3 files.

--- blackhack/src/blackhack/BlackHack.java
+++ blackhack/src/blackhack/BlackHack.java
@@ -5,42 +5,31 @@ import java.util.Scanner;
 public class BlackHack {
 
 	public static void main(String[] args) {
-
+		Player p = new Player();
+		final DBConnector db = new DBConnector();
 		System.out.println("Welcome to Blackhack!\n[1]Login\n[2]Register\n[3]Exit");
 
 		Scanner inp = new Scanner(System.in);
 		String choice = inp.next();
 		String username;
 		String password;
-		int fullclearence = 0; // S�tts till 1 eller 0 returneras fr�n metod i databaser.
-		int accepted = 0; // S�tts till 1 eller 0 returneras fr�n en metod i databaser.
-		
-		
+
 		while (true) 
 		{
 			if (choice.equals("1"))
 			{
 				while(true) 
 				{
-					// Kod f�r inloggning
-					System.out.println("Enterusername: ");
+					System.out.println("Enter username: ");
 					username = inp.next();
-					// Kolla om anv�ndarnamn redan �r taget
-					if (accepted == 1)
+					System.out.println("Enter password: ");
+					password = inp.next();
+					p.setCredentials(username, password);
+					
+					if (db.login(p) == 1)
 					{
-						System.out.println("Enter password: ");
-						password = inp.next();
-						// kolla om l�senord st�mmer med anv�ndare
-						if (fullclearence == 1)
-						{
-							System.out.println("Welcome! " + username + " Have fun Blackhacking!");
-							break;
-						}
-						else
-						{
-							System.out.println("Wrong password!");
-							break;
-						}
+						System.out.println("Welcome "+ p.getUsername()+"!");
+						break;
 					}
 					else
 					{
@@ -58,31 +47,19 @@ public class BlackHack {
 				{
 					System.out.println("Enter desired Username: ");
 					username = inp.next();
-					
-					if(accepted==1)
-					{
-				
 					System.out.println("Enter desired Password: ");
 					password = inp.next();
+					p.setCredentials(username, password);
 					
-					if(fullclearence==1)
-					{
-						System.out.println(username + " your account has been created, have fun playing!");
-						break;
+					if (db.registerPlayer(p) == 1) {
+						System.out.println("You have successfully created your account!\n Account name: "+username);
 					}
-					else
-						System.out.println("no password entered");
-						break;
+					else {
+						System.out.println("Username already taken!");
+					}
 				
-					}
-				else
-					System.out.println("Username exists already!");
-					break;
 				}	
 				
-				
-				
-				// kod f�r registrering
 			}
 			else if (choice.equals("3"))
 			{
