5a6ba0d5b9251e25214e4c85cba099ac3e7aef75
Sebastian Edholm <sebastian.edholm@gmail.com>
2018-12-23 12:00:21 +0100

    moved some gamelogic to player class

--- blackhack/src/blackhack/BlackHack.java
+++ blackhack/src/blackhack/BlackHack.java
@@ -6,21 +6,6 @@ public class BlackHack {
 	public static void main(String[] args) {
 		Game g = new Game();
 		//g.start();		
-		g.gamestart();
-
+		g.gameStart();
 	}
-
-
 }
-		
-		// Huvudmeny f�r programmet, som ligger i en loop
-		// Inoggning & registrering f�r att kunna spela.
-		// Detta skall kollas mot en databas
-		
-		// Ytterligare en loop n�r man har loggat in.
-		// H�r ska spelaren f� en ny meny f�r att kunna v�lja:
-		// Spela, kolla crediter, avsluta spel. (kanske lite regler om man vill...)
-		
-		// crediter ska kollas fr�n en databas och skriver ut p� sk�rmen.
-		// En undermeny f�r spela b�r finnas, d�r det fr�gas hur mycket man vill satsa innan man f�r kort.
-
--- blackhack/src/blackhack/Player.java
+++ blackhack/src/blackhack/Player.java
@@ -1,8 +1,40 @@
 package blackhack;
 
+import java.util.ArrayList;
+
 public class Player {
 	private String username;
 	private String password;
+	private int credits;
+
+	private ArrayList<Card> hand = new ArrayList<Card>();
+
+	public int getPoints(){
+		int sum = 0;
+		int numberofA = 0;
+
+		for(int i = 0; i < this.hand.size(); i++){
+			Card c = this.hand.get(i);
+			if(c.newRank() >= 2 && c.newRank() <= 10){
+				sum += c.newRank();
+			}
+			else if(c.newRank() == 11){
+				numberofA ++;
+			}
+		}
+
+		if(numberofA != 0){
+			if(sum + (numberofA*11) > 21){
+				sum += numberofA;
+			}
+			else{
+				sum += 11;
+			}
+		}
+		
+		return sum;
+
+	}
 
 	public void setCredentials(String username, String password) {
 		this.username = username;
@@ -23,5 +55,25 @@ public class Player {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+
+	public void setHand(ArrayList<Card> hand) {
+		this.hand = hand;
+	}
+	
+	public void addCard(Card c) {
+		this.hand.add(c);
+	}
+
+	public int getCredits() {
+		return credits;
+	}
+
+	public void setCredits(int credits) {
+		this.credits = credits;
 	}
 }
